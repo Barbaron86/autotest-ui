7f0fc1aba33a4799977814f43c99ac144b183da9
Barbaron86 <kirill.molodykh@mail.ru>
2026-08-27 23:27:25 +0300

    Знакомство с PageObject

---
+++ pages/base_page.py
@@ -0,0 +1,12 @@
+from playwright.sync_api import Page
+
+
+class BasePage:
+    def __init__(self, page: Page):
+        self.page = page
+
+    def visit(self, url: str) -> None:
+        self.page.goto(url)
+
+    def reload(self) -> None:
+        self.page.reload()
