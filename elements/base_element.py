504d2c297c1a1957da7d15f7e9e4bc837845dac7
Barbaron86 <kirill.molodykh@mail.ru>
2026-09-09 22:33:16 +0300

    test: Логирование взаимодействий со страницей

--- elements/base_element.py
+++ elements/base_element.py
@@ -1,5 +1,8 @@
 import allure
+from loguru import logger
 from playwright.sync_api import Locator, Page, expect
+
+logger = logger.bind(component="BASE_ELEMENT")
 
 
 class BaseElement:
@@ -14,20 +17,32 @@ class BaseElement:
 
     def get_locator(self, nth: int = 0, **kwargs: str | int) -> Locator:
         locator = self.locator.format(**kwargs)
-        with allure.step(f'Getting locator with "data-testid: {locator}" at index "{nth}"'):
+        step = f'Getting locator with "data-testid: {locator}" at index "{nth}"'
+
+        with allure.step(step):
+            logger.debug(step)
             return self.page.get_by_test_id(locator).nth(nth)
 
     def click(self, nth: int = 0, **kwargs: str | int) -> None:
-        with allure.step(f'Clicking on {self.type_of} "{self.name}"'):
+        step = f'Clicking on {self.type_of} "{self.name}"'
+
+        with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
+            logger.info(step)
             locator.click()
 
     def check_visible(self, nth: int = 0, **kwargs: str | int) -> None:
-        with allure.step(f'Checking that {self.type_of} "{self.name}" is visible'):
+        step = f'Checking that {self.type_of} "{self.name}" is visible'
+
+        with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
+            logger.info(step)
             expect(locator).to_be_visible()
 
     def check_have_text(self, text: str, nth: int = 0, **kwargs: str | int) -> None:
-        with allure.step(f'Checking that {self.type_of} "{self.name}" has text: "{text}"'):
+        step = f'Checking that {self.type_of} "{self.name}" has text: "{text}"'
+
+        with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
+            logger.info(step)
             expect(locator).to_have_text(text)
